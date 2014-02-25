e2cc5b54fcf33912d817b0c3a396ce230277975e
Sean Reed <seanreed1111@gmail.com>
2014-02-24 19:22:57 -0500

    adds overview of basic strategy logic

--- Basic Strategy Images/Basic Strategy Ruby Hashes/SD-H17-noDAS.rb
+++ Basic Strategy Images/Basic Strategy Ruby Hashes/SD-H17-noDAS.rb
@@ -1,47 +1,40 @@
 sdh17nodas ={}
 
-#ensure no matter what that if a person doubles down on a hand they get one card only
+#preprocessing needed for first two cards
+  # 1) split_allowed?
+  # 2) DAS_allowed?
+  # 3)double_down_allowed?
+  # 4) is_21? => if hand.total is 21, no further action needed
+  # 5)
 
+#dealer.hand.cards.first is the dealer's up card. 
+#  define     first = dealer.hand.cards.first.value in [1,2,3,4,5,6,7,8,9,10]
+#             total = player.hand.total
 
+##### if player.hand has_ace?
+#####     
+####      use split table if split_allowed? && total == 12 (player has AA and should split)
+####      use soft hand table
+####  else
+####      
+####      if player.hand.cards.first.value == player.hand.cards.second.value (check for pairs. )
+####         compute total value in [4,6,8,10,12,14,16,18,20]
 
-#has the player already split? 
-#only one split allowed. 
-#no resplitting aces
-####Then no further splitting is allowed
-####this needs to be a control function rather than something that lives
-####in the database to prevent unnecessary duplication
-#### override the result returned to next_move if it spli
-
-#has the player already doubled down?
-#### then no further cards are given to the player
-####this needs to be a control function rather than something that lives
-####in the database to prevent unnecessary duplication
-
-
-#dealer up card is first key - 
-######value in [1,2,3,4,5,6,7,8,9,10]
-
-# second key
-##### if :player has_ace? ==true 
-#####     boolean :are both cards aces?
-#####         if true
-####            split aces or hit if already split or split is not allowed
-####          else
-####            compute total value in [13,14,15,16,17,18,19,20,21]
-####            figure out correct move from SOFT HAND basic strategy and dealer card
-####          end
+####         if double_down_allowed?
+####            use double table for value = 8 or 10 (44, 55)
+####         end
+####         
+####         use split table if split_allowed? 
+####      else
+####         use hard hand table
+####      end
 ##### end
 
-#we now know that player has a hard hand. 
-# Need to rule out a split before using the hard hand table.
-# have already taken two aces into account
 
 
-#player has identical matching pair is the third key 
-
-# if card1.value==card2.value
-#### compute total = hand.total 
-#### total has value in [4,6,8,10,12,14,16,18,20]
-#### note: two aces have already been accounted for
-####if splits not allowed this key should be ignored or merged
-
+#preprocessing needed for first three cards
+#doubled_down?  => if hand has already been doubled down, no more cards allowed
+# if player has already split,
+##### update split_allowed? depending on if multiple splits
+##    are possible. 
+####  Also ensure that hand cannot resplit AA a second time
